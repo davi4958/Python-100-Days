f8cdee787b365c2dd4ebb1e2e0c8929d13d9ff4c
davi4958 <davi4958@users.noreply.github.com>
2020-12-01 09:22:49 +0800

    Create 123.py

---
+++ Day01-15/code/Day01/123.py
@@ -0,0 +1,31 @@
+import turtle
+
+
+def draw_rectangle(x, y, width, height):
+    """绘制矩形"""
+    turtle.goto(x, y)
+    turtle.pencolor('red')
+    turtle.fillcolor('red')
+    turtle.begin_fill()
+    for i in range(2):
+        turtle.forward(width)
+        turtle.left(90)
+        turtle.forward(height)
+        turtle.left(90)
+    turtle.end_fill()
+
+
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('red')
+
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+
+turtle.mainloop()
